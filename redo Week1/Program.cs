3c3c4ed5738eed399fd5e159a4b2c86ca72bdc3b
DavidBcyber <DBustos28@amarillocollege.com>
2025-04-14 14:04:37 -0500

    Add project files.

---
+++ redo Week1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string firstName, lastName, Color;
+            int age;
+            Console.WriteLine("What is your first name?"); // there first name
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?"); // there last name
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your age"); // there age
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite color"); // fav color
+            Color = Console.ReadLine();
+
+
+            // print out the confirmed the information message
+            Console.WriteLine("Hello " + firstName + " " + lastName + " you are " + age + " years old. Your favorite color is " + Color);
+        }
+    }
+}
